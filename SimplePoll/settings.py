0b90fb9c769b23ab661ec4b7be2c6c76a2a4bb44
saitheja007 <140613461+saitheja007@users.noreply.github.com>
2024-02-22 18:54:22 +0530

    Update settings.py

--- SimplePoll/settings.py
+++ SimplePoll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^y^it@riq*i==c!k7j6zvc6gf0x+z8fx86b^$fq0)vo@xktr)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
